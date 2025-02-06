eaa35522f1bbc553303afc36164758682d5fffff
Abiola Ibrahim <git@abiosoft.com>
2025-02-06 16:49:53 +0100

    dev: improve display for array items
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- frontend/src/lib/Node.svelte
+++ frontend/src/lib/Node.svelte
@@ -2,12 +2,19 @@
     import { onMount } from "svelte";
     import Node from "./Node.svelte";
 
+    const ARRAY_KEY = "__array__";
+
     let { key, node } = $props();
+
     let nodeEntries = Object.entries(node);
+    let nodeIsArray = $derived(Array.isArray(node));
     let hasChildren = $derived.by(() => {
         console.log("derived by", key, typeof node, node);
         return typeof node === "object" && node !== null;
     });
+
+    let itemIsArray = key == ARRAY_KEY;
+
     let isOpen = $state(false);
 
     function toggle() {
@@ -26,17 +33,36 @@
         {:else}
             <span class="toggle-icon">&nbsp; &nbsp;</span>
         {/if}
-        <span class="key">{key}</span>
-        {#if !hasChildren}
-            <input class="value" value={node} readonly="readonly" />
-        {/if}
+        <div class="key-value-container">
+            {#if itemIsArray}
+                <span>
+                    &bullet;
+                    <input
+                        class="value-only"
+                        value={node}
+                        readonly="readonly"
+                    />
+                </span>
+            {:else}
+                <span class="key">{key}</span>
+                {#if !hasChildren}
+                    <input class="value" value={node} readonly="readonly" />
+                {/if}
+            {/if}
+        </div>
     </div>
 
     {#if hasChildren && isOpen}
         <div class="children">
-            {#each nodeEntries as [key, node]}
-                <Node {key} {node} />
-            {/each}
+            {#if nodeIsArray}
+                {#each node as node}
+                    <Node key={ARRAY_KEY} {node} />
+                {/each}
+            {:else}
+                {#each nodeEntries as [key, node]}
+                    <Node {key} {node} />
+                {/each}
+            {/if}
         </div>
     {/if}
 </div>
@@ -44,6 +70,8 @@
 <style>
     .tree-node {
         margin-left: 20px;
+        display: flex;
+        flex-direction: column;
     }
     .node-header {
         cursor: pointer;
@@ -55,19 +83,34 @@
         font-size: 10px;
     }
 
-    .key {
-        margin-right: 10px;
-        font-family: monospace;
+    .key-value-container {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
     }
 
-    .value {
+    .key {
         flex: 1;
+        margin-right: 10px;
+        font-family: monospace;
+        padding: 5px;
+    }
+
+    .value,
+    .value-only {
+        flex: 2;
         margin-left: 10px;
         border: none;
         background: transparent;
         font-family: monospace;
         font-size: inherit;
         color: inherit;
+        padding: 5px;
+        border-radius: 4px;
+    }
+
+    .value-only {
+        flex: 1;
     }
 
     .children {
