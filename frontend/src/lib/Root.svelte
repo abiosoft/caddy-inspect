7be629fe523ee3812fdaa25640a782d1b9e4d037
Abiola Ibrahim <git@abiosoft.com>
2025-02-10 13:23:00 +0100

    frontend: display Caddyfile snippet
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- frontend/src/lib/Root.svelte
+++ frontend/src/lib/Root.svelte
@@ -9,8 +9,12 @@
     let treeData = $state([]);
     let hasData = $derived(treeData.length > 0);
     let requestUrl = $state("");
+    let requestMethod = $state("");
     let sourceFile = $state("");
+    let sourceLine = $state(0);
+    let sourceLineStart = $state(0);
     let hasSourceFile = $derived(sourceFile != "");
+    let sources = $state([]);
 
     let intervalId = $state(0);
     let currentRequestId = $state(0);
@@ -27,7 +31,7 @@
 
     $effect(() => {
         document.title = !!requestUrl
-            ? `Caddy Inspect - ${requestUrl}`
+            ? `Caddy Inspect - ${requestMethod.toUpperCase()} ${requestUrl}`
             : "Caddy Inspect";
     });
 
@@ -44,8 +48,12 @@
     function resetRequests() {
         treeData = [];
         requestUrl = "";
+        requestMethod = "";
         currentRequestId = 0;
         sourceFile = "";
+        sourceLine = 0;
+        sourceLineStart = 0;
+        sources = [];
     }
 
     async function fetchRequests() {
@@ -69,15 +77,19 @@
         // request id
         currentRequestId = data.id;
 
-        // tree rendering data
-        treeData = Object.entries(request);
-
         // other rendering properties
         requestUrl = data.request.url;
+        requestMethod = data.request.method;
         hasResponse = data.has_response;
         if (caddyfile) {
-            sourceFile = `${caddyfile.file}:${caddyfile.line}`;
+            sourceFile = caddyfile.file;
+            sourceLine = caddyfile.line;
+            sourceLineStart = caddyfile.source_line_start;
+            sources = caddyfile.source;
         }
+
+        // tree rendering data
+        treeData = Object.entries(request);
 
         window.focus();
     }
@@ -150,7 +162,34 @@
         <div class="tree json-tree">
             {#if hasSourceFile}
                 <div class="source-file">
-                    &#9673 <code>{sourceFile}</code>
+                    <code> {sourceFile}:{sourceLine}</code>
+                </div>
+                <div class="snippet">
+                    {#each sources as line, i}
+                        <div
+                            class={{
+                                "code-line": true,
+                                highlight: i + sourceLineStart == sourceLine,
+                            }}
+                        >
+                            <span class="mark">
+                                {#if i + sourceLineStart == sourceLine}
+                                    {#if hasResponse}
+                                        &uarr;
+                                    {:else}
+                                        &darr;
+                                    {/if}
+                                    &#9673
+                                {/if}
+                            </span>
+                            <span class="line-number"
+                                >{i + sourceLineStart}</span
+                            >
+                            <span class="line-content">{line}</span>
+                        </div>
+                    {:else}
+                        &nbsp;
+                    {/each}
                 </div>
             {/if}
 
@@ -166,5 +205,103 @@
 <style>
     .tree {
         font-family: Arial, sans-serif;
+    }
+
+    .json-tree {
+        font-family: monospace;
+        font-size: 15px;
+        padding: 20px;
+        border: 1px solid var(--border-color);
+        border-radius: 4px;
+        background-color: var(--textarea-bg-color);
+        color: var(--text-color);
+    }
+
+    .container {
+        max-width: 800px;
+        margin: 20px auto;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        background-color: var(--bg-color);
+    }
+
+    .top-row {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+    .top-row-item {
+        padding: 5px;
+    }
+
+    .top-row-item button {
+        width: 50px;
+        font-size: 25px;
+        padding: 0;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        line-height: 1.2;
+    }
+
+    .top-row-right {
+        margin-left: auto;
+    }
+
+    .header {
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        font-size: 20px;
+        font-weight: bold;
+        color: var(--text-color);
+    }
+
+    .source-file {
+        text-align: center;
+        font-size: 12px;
+        font-weight: bold;
+        padding-bottom: 10px;
+    }
+
+    .snippet {
+        display: flex;
+        flex-direction: column;
+        font-family: monospace;
+        font-size: 13px;
+        padding: 10px;
+        border-radius: 5px;
+        overflow: auto;
+        border: solid 1px var(--calm-color);
+        margin-bottom: 10px;
+    }
+
+    .code-line {
+        display: flex;
+        padding: 2px 5px;
+    }
+    .mark {
+        width: 30px;
+        text-align: right;
+        margin-right: 0;
+    }
+    .highlight {
+        background-color: var(--snippet-highlight-color);
+    }
+    .line-number {
+        width: 30px;
+        text-align: right;
+        margin-right: 10px;
+        color: var(--calm-color);
+    }
+    .highlight .line-number {
+        color: var(--text-color);
+    }
+
+    .line-content {
+        white-space: pre;
     }
 </style>
